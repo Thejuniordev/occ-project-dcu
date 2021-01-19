6fffa17116702a39cc3f286b60b94612d18cb914
Darcy Junior <darcy.santos@compasso.com.br>
2021-01-19 09:13:53 -0300

    Remoção de console log

--- src/dcu.js
+++ src/dcu.js
@@ -4,7 +4,6 @@ const { CONSTANTS } = require('./constants');
 const { occEnv } = require('./occEnv');
 
 const DCU_BASE_COMMAND = `node ${CONSTANTS.PATHS.DCU} -b ${CONSTANTS.PATHS.SRC} -n ${process.env.OCC_ADMIN_URL} -k ${process.env.OCC_APP_KEY}`;
-const DCU_BASE_PUT = `node ${CONSTANTS.PATHS.DCU} -k ${process.env.OCC_APP_KEY} -n ${process.env.OCC_ADMIN_URL}`;
 
 const Methods =  {
     grab: (adminUrl, appKey) => {
@@ -21,14 +20,12 @@ const Methods =  {
         shell.exec(`${DCU_BASE_COMMAND} -t "${file}"`, {
             async: false,
         });
-        console.log('Meu file: ', file);
     },
 
     refresh: path => {
         shell.exec(`${DCU_BASE_COMMAND} -e "${path.replace(/\/$/g, '')}"`, {
             async: false,
         });
-        console.log('Meu file: ', path);
     },
     
     putAll: path => {
